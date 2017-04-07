2b20f7b98415fe4f300de0fe108e93a507b71b30
tonythepooh <tony361@gmail.com>
2017-04-06 23:59:50 -0700

    4.3 Controllers Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,10 +61,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  def landing_page
-    @featured_product = Product.first
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
