5bd9070431fbedaa5e0458a2c9b4c0e8d4df25dc
tonythepooh <tony361@gmail.com>
2017-04-30 13:03:58 -0700

    5.2 add condition for Like and ilike

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,11 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	def self.search(search_term)
-		Product.where("name Like ?", "%#{search_term}%")
+			if Rails.env.development?
+			Product.where("name LIKE ?", "%#{search_term}%")
+		else
+			Product.where("name iLike ?", "%#{search_term}%")
+		end
 	end
 end
 
