f5fec06c0f5dcddc2a838d8d2a6366f75e96b5cf
tonythepooh <tony361@gmail.com>
2017-05-12 23:15:54 -0400

    edit user command

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id:user .id
+    can :manage, User, id:user.id
   
     # Define abilities for the passed in user here. For example:
     #
