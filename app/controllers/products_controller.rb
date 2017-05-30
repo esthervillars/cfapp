cddfad5c5d1b69a225d6035769a3c463f772d46d
tonythepooh <tony361@gmail.com>
2017-05-29 18:13:56 -0700

    put script then rating star showing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,7 +34,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to "/landing_page", notice: 'Product was successfully created.' }
+        format.html { redirect_to "/root_path", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
