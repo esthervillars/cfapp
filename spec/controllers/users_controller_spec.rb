10d1d38e52804b6e688896d7ae15a561fb06166c
tonythepooh <tony361@gmail.com>
2017-06-07 20:11:14 +0700

    6.3 write functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,21 +5,27 @@ describe UsersController, type: :controller do
   let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
 
   describe 'GET #show' do
-     context 'User is logged in' do
+     context 'when a user is logged in' do
      	before do
-     		sign_in user
+     		sign_in user1
      end
      	it "load correct user details" do
-     		get:show, id: user.id
+     		get:show, params: {id: user1.id}
      		expect(response).to have_http_status(200)
-     		expect(assigns(:user)).to eq user
+     		expect(assigns(:user)).to eq user1
      	end
-    end
 
-     context 'No user is logged in' do
+      it "cannot access to other user" do
+        get:show, params: {id: user2.id}
+        expect(response).to redirect_to(root_path)
+      end
+    end
+      
+
+     context 'when a user is not logged in' do
        it 'redirects to login' do
-         get :show, id: user.id
-         expect(response).to redirect_to(root_path)
+         get :show, params: {id: user.id}
+         expect(response).to redirect_to(new_user_session_path)
        end
      end
   end
