429afab55a4792b936354b3b18767f22bbe381f7
tonythepooh <tony361@gmail.com>
2017-06-11 17:30:50 +0700

    fixed create paymant page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,22 +9,23 @@ class PaymentsController < ApplicationController
 	      amount: (@product.price*100).to_i, # amount in cents, again
 	      currency: "usd",
 	      source: token,
-	      description: params[:stripeEmail]
+	      description :params[:stripeEmail]
 	    )
 
 	    if charge.paid
 	    	Order.create(
 	    		user_id: @user.id,
-	    		product_id: params[:product_id],
-	    		total: @product.price)
+	    		product_id: @product_id,
+	    		total: @product.price
+	    		)
 	    	flash[:notice] = "Thank you for purchasing, Your payment was processed successfully."
 	    end
 
 	  rescue Stripe::CardError => e
 	    # The card has been declined
 	    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    	err = body[:error]
+    	flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
 	  end
 	  redirect_to product_path(@product)
 	end
